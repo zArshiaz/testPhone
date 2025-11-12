9cc16c7a9780dfab8bce776c5c4185258c9a80f9
Arshia Zinedine <arshia.zinedine@gmail.com>
2025-11-12 22:56:20 +0330

    5.0.0

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import open from "open";
 const min = 2;
 let count = 0;
 let token;
-const captcha ="03AIIukzi82j6kHP4CFcC_MIbaoGWKdkj3Akd_acQBULo9pjPIltV1ewSmMsetwp2tWd3b9g1i7CYhU3vK0_7_QzCytARHPEVQrUQiqDaK1CO5nJ99XwmuLVTj1cq7mnw5Z9WZEFXVy__Vi_useYMNExNcpb-fk3jTTv-Tx7xdJWZepo6K0LLPPlujjs4GKXMJn39q4EDGJafJ-RMLg9QzVbEZaBJKeZEertRyrnqMR-cvxHtRtrLhGhHLYEq9P5sijxE9dtt7ecuBtPUr68uuQaNG8zxIK3Lw3JQEBYtwcyLlEyvpTLXaQvgK6JyyY_AKVK4HspDHMjZLmZQipMAF92mDipgtshqebWwbdxS1D99dDPBznA3QvsXIL1yZ97s37L9ZvWlygOekEr7bMCNHbsvBF_VlsGrnWTP2NcQbWNv2E-WQEGs1Jwx2zet-dAlEHXlNYw_AjlhNa_b0GHpY2ttfLFr9WJWtZfg1lTvZo4gkOCXOEIi01htol0cGyG19kXMTivBn3WcQ";
+const captcha =
+  "03AIIukzi82j6kHP4CFcC_MIbaoGWKdkj3Akd_acQBULo9pjPIltV1ewSmMsetwp2tWd3b9g1i7CYhU3vK0_7_QzCytARHPEVQrUQiqDaK1CO5nJ99XwmuLVTj1cq7mnw5Z9WZEFXVy__Vi_useYMNExNcpb-fk3jTTv-Tx7xdJWZepo6K0LLPPlujjs4GKXMJn39q4EDGJafJ-RMLg9QzVbEZaBJKeZEertRyrnqMR-cvxHtRtrLhGhHLYEq9P5sijxE9dtt7ecuBtPUr68uuQaNG8zxIK3Lw3JQEBYtwcyLlEyvpTLXaQvgK6JyyY_AKVK4HspDHMjZLmZQipMAF92mDipgtshqebWwbdxS1D99dDPBznA3QvsXIL1yZ97s37L9ZvWlygOekEr7bMCNHbsvBF_VlsGrnWTP2NcQbWNv2E-WQEGs1Jwx2zet-dAlEHXlNYw_AjlhNa_b0GHpY2ttfLFr9WJWtZfg1lTvZo4gkOCXOEIi01htol0cGyG19kXMTivBn3WcQ";
 const BOT_TOKEN = "1981233539:9lTQs6eO-xC1lgRdj3Wb_69yN4riL23hyA4";
 const CHAT_ID = "1038621242";
 sendTelegram(`کد هر ${min} دقیقه یبار اجرا میشه `);
@@ -44,7 +45,8 @@ async function sendTelegram(text) {
   const url = `https://tapi.bale.ai/bot${BOT_TOKEN}/sendMessage`;
   const payload = {
     chat_id: CHAT_ID,
-    text  };
+    text,
+  };
 
   const res = await fetch(url, {
     method: "POST",
@@ -64,7 +66,6 @@ async function refreshToken() {
     if (newToken) {
       token = newToken;
       console.log("Token refreshed.");
-      await sendTelegram("توکن جدید ساخته شد");
     } else {
       console.warn("createApiToken returned falsy result.");
     }
@@ -78,29 +79,254 @@ async function refreshToken() {
 
 const bundles = [
   {
-    name: "Simin",
+    name: "Simin2",
     bundlesalessimid: "b3e351a2-980e-414f-b04a-f9467f4f4a9d",
-    regex: /^999106/,
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b3e351a2-980e-414f-b04a-f9467f4f4a9d&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "2******",
   },
   {
-    name: "Borons",
+    name: "Simin3",
+    bundlesalessimid: "b3e351a2-980e-414f-b04a-f9467f4f4a9d",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b3e351a2-980e-414f-b04a-f9467f4f4a9d&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "3******",
+  },
+  {
+    name: "Simin4",
+    bundlesalessimid: "b3e351a2-980e-414f-b04a-f9467f4f4a9d",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b3e351a2-980e-414f-b04a-f9467f4f4a9d&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "4******",
+  },
+  {
+    name: "Simin5",
+    bundlesalessimid: "b3e351a2-980e-414f-b04a-f9467f4f4a9d",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b3e351a2-980e-414f-b04a-f9467f4f4a9d&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "5******",
+  },
+  {
+    name: "Simin6",
+    bundlesalessimid: "b3e351a2-980e-414f-b04a-f9467f4f4a9d",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b3e351a2-980e-414f-b04a-f9467f4f4a9d&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "6******",
+  },
+  {
+    name: "Simin7",
+    bundlesalessimid: "b3e351a2-980e-414f-b04a-f9467f4f4a9d",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b3e351a2-980e-414f-b04a-f9467f4f4a9d&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "7******",
+  },
+  {
+    name: "Simin8",
+    bundlesalessimid: "b3e351a2-980e-414f-b04a-f9467f4f4a9d",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b3e351a2-980e-414f-b04a-f9467f4f4a9d&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "8******",
+  },
+  {
+    name: "Simin9",
+    bundlesalessimid: "b3e351a2-980e-414f-b04a-f9467f4f4a9d",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b3e351a2-980e-414f-b04a-f9467f4f4a9d&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "9******",
+  },
+
+  {
+    name: "Borons2",
     bundlesalessimid: "7ccd5a85-cb6e-46be-b3db-b398e6de85ba",
-    regex: /^999169/,
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=7ccd5a85-cb6e-46be-b3db-b398e6de85ba&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "2******",
   },
   {
-    name: "Silver",
+    name: "Borons3",
+    bundlesalessimid: "7ccd5a85-cb6e-46be-b3db-b398e6de85ba",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=7ccd5a85-cb6e-46be-b3db-b398e6de85ba&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "3******",
+  },
+  {
+    name: "Borons4",
+    bundlesalessimid: "7ccd5a85-cb6e-46be-b3db-b398e6de85ba",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=7ccd5a85-cb6e-46be-b3db-b398e6de85ba&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "4******",
+  },
+  {
+    name: "Borons5",
+    bundlesalessimid: "7ccd5a85-cb6e-46be-b3db-b398e6de85ba",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=7ccd5a85-cb6e-46be-b3db-b398e6de85ba&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "5******",
+  },
+  {
+    name: "Borons6",
+    bundlesalessimid: "7ccd5a85-cb6e-46be-b3db-b398e6de85ba",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=7ccd5a85-cb6e-46be-b3db-b398e6de85ba&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "6******",
+  },
+  {
+    name: "Borons7",
+    bundlesalessimid: "7ccd5a85-cb6e-46be-b3db-b398e6de85ba",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=7ccd5a85-cb6e-46be-b3db-b398e6de85ba&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "7******",
+  },
+  {
+    name: "Borons8",
+    bundlesalessimid: "7ccd5a85-cb6e-46be-b3db-b398e6de85ba",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=7ccd5a85-cb6e-46be-b3db-b398e6de85ba&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "8******",
+  },
+  {
+    name: "Borons9",
+    bundlesalessimid: "7ccd5a85-cb6e-46be-b3db-b398e6de85ba",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=7ccd5a85-cb6e-46be-b3db-b398e6de85ba&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "9******",
+  },
+
+  {
+    name: "Silver2",
     bundlesalessimid: "4cf89d9e-3bbd-41f6-a3b9-f725e8d789f5",
-    regex: /^999103/,
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=d6d535e7-71a8-469c-ad93-520c1dc4f8ec&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "2******",
   },
   {
-    name: "Gold",
+    name: "Silver3",
+    bundlesalessimid: "4cf89d9e-3bbd-41f6-a3b9-f725e8d789f5",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=d6d535e7-71a8-469c-ad93-520c1dc4f8ec&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "3******",
+  },
+  {
+    name: "Silver4",
+    bundlesalessimid: "4cf89d9e-3bbd-41f6-a3b9-f725e8d789f5",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=d6d535e7-71a8-469c-ad93-520c1dc4f8ec&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "4******",
+  },
+  {
+    name: "Silver5",
+    bundlesalessimid: "4cf89d9e-3bbd-41f6-a3b9-f725e8d789f5",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=d6d535e7-71a8-469c-ad93-520c1dc4f8ec&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "5******",
+  },
+  {
+    name: "Silver6",
+    bundlesalessimid: "4cf89d9e-3bbd-41f6-a3b9-f725e8d789f5",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=d6d535e7-71a8-469c-ad93-520c1dc4f8ec&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "6******",
+  },
+  {
+    name: "Silver7",
+    bundlesalessimid: "4cf89d9e-3bbd-41f6-a3b9-f725e8d789f5",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=d6d535e7-71a8-469c-ad93-520c1dc4f8ec&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "7******",
+  },
+  {
+    name: "Silver8",
+    bundlesalessimid: "4cf89d9e-3bbd-41f6-a3b9-f725e8d789f5",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=d6d535e7-71a8-469c-ad93-520c1dc4f8ec&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "8******",
+  },
+  {
+    name: "Silver9",
+    bundlesalessimid: "4cf89d9e-3bbd-41f6-a3b9-f725e8d789f5",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=d6d535e7-71a8-469c-ad93-520c1dc4f8ec&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "9******",
+  },
+
+  {
+    name: "Gold2",
     bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
-    regex: /^999101/,
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "2******",
   },
   {
-    name: "Plutonium",
+    name: "Gold2",
+    bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "2******",
+  },
+  {
+    name: "Gold3",
+    bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "3******",
+  },
+  {
+    name: "Gold4",
+    bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "4******",
+  },
+  {
+    name: "Gold5",
+    bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "5******",
+  },
+  {
+    name: "Gold6",
+    bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "6******",
+  },
+  {
+    name: "Gold7",
+    bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "7******",
+  },
+  {
+    name: "Gold8",
+    bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "8******",
+  },
+  {
+    name: "Gold9",
+    bundlesalessimid: "6d88df11-9a2d-4eb8-affa-335b1730dbc2",
+    href: "https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "9******",
+  },
+
+  {
+    name: "Plutonium2",
     bundlesalessimid: "02414558-e7c9-4ea9-a68e-9ed47058141e",
-    regex: /^999101/,
+    href: "href:'https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "2******",
+  },
+  {
+    name: "Plutonium3",
+    bundlesalessimid: "02414558-e7c9-4ea9-a68e-9ed47058141e",
+    href: "href:'https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "3******",
+  },
+  {
+    name: "Plutonium4",
+    bundlesalessimid: "02414558-e7c9-4ea9-a68e-9ed47058141e",
+    href: "href:'https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "4******",
+  },
+  {
+    name: "Plutonium5",
+    bundlesalessimid: "02414558-e7c9-4ea9-a68e-9ed47058141e",
+    href: "href:'https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "5******",
+  },
+  {
+    name: "Plutonium6",
+    bundlesalessimid: "02414558-e7c9-4ea9-a68e-9ed47058141e",
+    href: "href:'https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "6******",
+  },
+  {
+    name: "Plutonium7",
+    bundlesalessimid: "02414558-e7c9-4ea9-a68e-9ed47058141e",
+    href: "href:'https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "7******",
+  },
+  {
+    name: "Plutonium8",
+    bundlesalessimid: "02414558-e7c9-4ea9-a68e-9ed47058141e",
+    href: "href:'https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "8******",
+  },
+  {
+    name: "Plutonium9",
+    bundlesalessimid: "02414558-e7c9-4ea9-a68e-9ed47058141e",
+    href: "href:'https://simcardstore.aptel.ir/simstore/html/select-number.html?bundleid=b7dc1e3b-7893-4baf-b230-65a9c2ecde03&gift=1af1947c-bfc4-45a9-8769-b93a65e9a51b",
+    filter: "9******",
   },
 ];
 
@@ -109,7 +335,6 @@ let soundPlaying = false;
 
 async function checkNumbers(bundle) {
   count += 1;
-  console.log("count fetch: ", count);
 
   async function attemptFetch(allowRetry = true) {
     try {
@@ -126,7 +351,7 @@ async function checkNumbers(bundle) {
           body: JSON.stringify({
             searchconfigmodel: {
               sort: "",
-              filter: "*******",
+              filter: bundle.filter,
               entriesperpage: 20,
               page: 0,
               totalrowcount: 0,
@@ -145,24 +370,25 @@ async function checkNumbers(bundle) {
       const result = await response.json();
       const phones = result?.Result?.map((d) => d.msisdn) || [];
 
-      for (let phone of phones) {
-        if (!bundle.regex.test(phone)) {
-          console.log(`Match found (${bundle.name}):`, phone);
-          sendTelegram(
-            ' _بدو شماره جدید اومد<a href="https://simcardstore.aptel.ir/simstore/html/landing.html">بریم</a>'
-          ).catch((e) => console.error(e));
-          open("https://simcardstore.aptel.ir/simstore/html/landing.html");
-          player.play("sound/ok.mp3", (err) => {
-            if (err) console.error("Sound error:", err);
-          });
-          if (intervals[bundle.name]) {
-            clearInterval(intervals[bundle.name]);
-            sendTelegram(`اینترول ${bundle.name} حذف شد`);
-          }
-          return;
+      if (phones.length > 0) {
+        console.log(`Match found (${bundle.name}):`, phones);
+        sendTelegram(
+          `تو بخش ${bundle.name} شماره جدید اومد ${
+            bundle.href
+          }    ${phones.join("_")}`
+        ).catch((e) => console.error(e));
+        open(bundle.href);
+        player.play("sound/ok.mp3", (err) => {
+          if (err) console.error("Sound error:", err);
+        });
+        if (intervals[bundle.name]) {
+          clearInterval(intervals[bundle.name]);
+          sendTelegram(`اینترول ${bundle.name} حذف شد`);
         }
+        return;
       }
-      console.log(`No match for ${bundle.name}`);
+
+      console.log(`No match for ${bundle.name}__`, count);
     } catch (err) {
       console.error(`Request failed for ${bundle.name}:`, err);
 
@@ -171,15 +397,11 @@ async function checkNumbers(bundle) {
           if (e) console.error("Sound error:", e);
         });
         await sendTelegram("کدت خطا خورد");
-         if (intervals[bundle.name]) {
-            clearInterval(intervals[bundle.name]);
-            sendTelegram(`اینترول ${bundle.name} حذف شد`);
-          }
       }
 
       if (allowRetry) {
         await refreshToken();
-        return attemptFetch(false);
+        return attemptFetch();
       }
     }
   }
@@ -196,15 +418,16 @@ async function init() {
   }
 
   bundles.forEach((bundle, index) => {
-    setTimeout(() => checkNumbers(bundle), index * 3000);
+    setTimeout(() => checkNumbers(bundle), index * 2000);
 
     setTimeout(() => {
       intervals[bundle.name] = setInterval(
         () => checkNumbers(bundle),
         min * 60 * 1000
       );
-    }, index * 3000);
+    }, index * 2000);
   });
+
 }
 
 init();
